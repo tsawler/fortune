7b381372cd8ef863848306ed1992486f62e3a0cb
Trevor Sawler <trevor.sawler@gmail.com>
2020-10-29 20:23:41 -0300

    Simplify

--- fortune.go
+++ fortune.go
@@ -2,7 +2,6 @@ package fortune
 
 import (
 	"encoding/json"
-	"fmt"
 	"math/rand"
 	"net/http"
 	"time"
@@ -41,45 +40,16 @@ func allFortunes() ([]Fortune, error) {
 
 // RandomFortune returns one fortune and an error, if any
 func RandomFortune() (string, error) {
-	var af Fortune
-
+	// call allFortunes to get all fortunes into a slice
 	fortuneSlice, err := allFortunes()
 	if err != nil {
 		return "", err
 	}
 
+	// seed our pseudo random generator
 	rand.Seed(time.Now().UnixNano())
-	randomFortuneID := rand.Intn(len(fortuneSlice))
 
-	fortuneToGet := fortuneSlice[randomFortuneID]
-
-	// we actually have everything we need by this point, but will get get one fortune by
-	// id just to demonstrate that the http client can be used more than once
-	af, err = getFortuneByID(fortuneToGet.ID)
-	if err != nil {
-		return "", err
-	}
-
-	return af.Message, nil
-}
-
-// getFortuneByID gets one fortune by ID
-func getFortuneByID(id string) (Fortune, error) {
-	var fortune Fortune
-
-	resp, err := myClient.Get(fmt.Sprintf("%s/%s", url, id))
-	if err != nil {
-		return fortune, err
-	}
-	defer func() {
-		_ = resp.Body.Close()
-	}()
-
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&fortune)
-	if err != nil {
-		return fortune, err
-	}
-
-	return fortune, nil
+	// get a random fortune from slice
+	myFortune := fortuneSlice[rand.Intn(len(fortuneSlice))]
+	return myFortune.Message, nil
 }

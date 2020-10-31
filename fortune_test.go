19fe5018625d6da6f97c1ca49749c0c8849b6588
Trevor Sawler <trevor.sawler@gmail.com>
2020-10-31 13:07:35 -0300

    Update readme

--- fortune_test.go
+++ fortune_test.go
@@ -1,7 +1,13 @@
 package fortune
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -18,6 +24,54 @@ func NewTestClient(fn RoundTripFunc) *http.Client {
 	}
 }
 
-func testAllFortunes(t *testing.T) {
+func TestRandomFortune(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
 
+		return &http.Response{
+			StatusCode: 200,
+			Body: ioutil.NopCloser(bytes.NewBufferString(`
+[
+   {
+      "message": "A friend's frown is better than a fool's smile.",
+      "id": "5403c81dc2fea4020029ab34"
+   }
+]
+`)),
+			Header: make(http.Header),
+		}
+	})
+
+	api := API{client, "http://example.com"}
+	body, err := api.RandomFortune()
+
+	ok(t, err)
+	equals(t, "A friend's frown is better than a fool's smile.", body)
+
+}
+
+// assert fails the test if the condition is false.
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// ok fails the test if an err is not nil.
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
 }
